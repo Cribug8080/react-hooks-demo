fa5a03a980bb45d5dd6894e0d18657304db2097d
八千鸟 <baqianbird@outlook.com>
2022-12-28 09:49:30 +0000

    Update Virtual.jsx

--- src/Pages/Virtual.jsx
+++ src/Pages/Virtual.jsx
@@ -1,26 +1,39 @@
 import { useRef } from 'react';
 import { useVirtualList } from 'ahooks';
+import { Select, InputNumber } from 'antd';
 
 const originList = Array.from(Array(9999).keys());
+const options = new Array(5).fill(1).map((v, i) => ({
+    label: i,
+    value: i
+}));
 
 export default () => {
     const containerRef = useRef(null);
     const wrapperRef = useRef(null);
 
-    const [list] = useVirtualList(originList, {
+    const [list, scrollTo] = useVirtualList(originList, {
         containerTarget: containerRef,
         wrapperTarget: wrapperRef,
         itemHeight: 60,
         overscan: 10,
     });
     return (
-        <div ref={containerRef} style={{ height: '300px', border: '1px solid' }}>
-            <div ref={wrapperRef}>
-                {
-                    list.map(v => (
-                        <div style={{ height: '58px', border: '1px solid' }}>{v.data}</div>
-                    ))
-                }
+        <div>
+            <div style={{ textAlign: 'right' }}>
+                scroll to2 <InputNumber onChange={scrollTo} />
+            </div>
+            <div ref={containerRef} style={{ height: '300px', border: '1px solid', overflow: 'auto' }}>
+                <div ref={wrapperRef}>
+                    {
+                        list.map(v => (
+                            <div style={{ height: '58px', border: '1px solid' }}>
+                                {v.data}
+                                <Select options={options} />
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
